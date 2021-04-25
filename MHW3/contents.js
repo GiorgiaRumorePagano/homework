96e504b67b692f559adfd23d176f38a911565b8b
GiorgiaRumorePagano <79807991+GiorgiaRumorePagano@users.noreply.github.com>
2021-04-25 12:47:21 +0200

    Add files via upload

---
+++ MHW3/contents.js
@@ -0,0 +1,80 @@
+const contenuti = [
+{
+    titolo: 'Nuoto Libero',
+    immagine: 'https://images.pexels.com/photos/4751260/pexels-photo-4751260.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    dettagli: 'Viene a nuotare insieme alla foca Agatina'
+},
+
+{
+    titolo: 'Scuola Nuoto',
+    immagine: 'https://images.pexels.com/photos/68744/loggerhead-turtle-sea-ocean-water-68744.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    dettagli: 'Impara a nuotare come Beluga la Tartaruga'
+},
+
+{
+    titolo: 'Scuola Nuoto Adulti',
+    immagine: 'https://images.pexels.com/photos/64219/dolphin-marine-mammals-water-sea-64219.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    dettagli: 'Impara a nuotare come Peppino il Delfino'
+},
+
+{
+    titolo: 'Pallanuoto',
+    immagine: 'https://cdn.pixabay.com/photo/2018/10/07/17/04/dolphin-3730702_1280.jpg',
+    dettagli: 'Fai come Toto, fatti una partita a Pallanuoto'
+},
+
+{
+    titolo: 'Nuoto Paralimpico',
+    immagine: 'https://images.pexels.com/photos/4047457/pexels-photo-4047457.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    dettagli: 'Attrezzature adeguate e spogliatoi accessibili a tutti.'
+},
+
+{
+    titolo: 'Acquafitness',
+    immagine: 'https://images.pexels.com/photos/66258/staniel-cay-swimming-pig-seagull-fish-66258.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    dettagli: 'Peppa che fa il bagnetto'
+},
+
+{
+    titolo: 'Sala Pesi',
+    immagine: 'https://i0.wp.com/www.gruppoitalianocivette.it/wp-content/uploads/2018/04/fun.jpg',
+    dettagli: 'Aiuta Lulù, non ce la fa più...'
+},
+
+{
+    titolo: 'Funzionale',
+    immagine: 'https://t1.ea.ltmcdn.com/it/images/5/6/2/cane_che_ringhia_per_gioco_cosa_significa_2265_orig.jpg',
+    dettagli: 'più informazioni'
+},
+
+{
+    titolo: 'Pugilato',
+    immagine: 'https://images6.alphacoders.com/348/thumb-1920-348200.jpg',
+    dettagli: 'Kung Fu Panda!'
+},
+
+{
+    titolo: 'Danza',
+    immagine: 'https://www.treehugger.com/thmb/eEh-Fi0S-liTM1wxpmAjrhnjtzk=/644x364/filters:fill(auto,1)/__opt__aboutcom__coeus__resources__content_migration__mnn__images__2016__04__dog-dancing-827e808b5ad54fbfbe801d57c8ffda54.jpg',
+    dettagli: 'Tunz Tunz Tunz'
+},
+
+{
+    titolo: 'Zumba',
+    immagine: 'https://www.focusjunior.it/content/uploads/2018/04/animali-che-ballano-lemure_25.jpg',
+    dettagli: 'La mano arriba, cintura sola'
+},
+
+{
+    titolo: 'Ginnastica',
+    immagine: 'https://images.pexels.com/photos/979247/pexels-photo-979247.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    dettagli: 'Vita da Gatti'
+},
+
+{
+    titolo: 'Preparazione calciatori',
+    immagine: 'https://images.pexels.com/photos/6550874/pexels-photo-6550874.jpeg?auto=compress&cs=tinysrgb&dpr=3&h=750&w=1260',
+    dettagli: 'Prepare i tuoi muscoli per affrontare al meglio le partite.'
+}
+
+]
---
+++ MHW3/script.js
@@ -0,0 +1,276 @@
+
+const section = document.querySelector('#section_corsi');
+const divcorsi = document.createElement('div');
+divcorsi.classList.add('corsi');
+section.appendChild(divcorsi);
+
+for (var i = 0; i < contenuti.length; i++) {
+    const divcorso = document.createElement('div');
+    divcorso.id = ('corso' + i);
+    divcorsi.appendChild(divcorso);
+    const span = document.createElement('span');
+    span.classList.add('row');
+    divcorso.appendChild(span);
+    const titolo = document.createElement('h3');
+    titolo.textContent = contenuti[i].titolo;
+    span.appendChild(titolo);
+    const button = document.createElement('span');
+    button.classList.add('button');
+    button.textContent = "Aggiungi ai preferiti";
+    button.addEventListener('click', onFavorites);
+    span.appendChild(button);
+    const img = document.createElement('img');
+    img.src = contenuti[i].immagine;
+    divcorso.appendChild(img);
+    const dettagli = document.createElement('p');
+    dettagli.classList.add('hidden');
+    dettagli.classList.add('inf');
+    dettagli.textContent = contenuti[i].dettagli;
+    divcorso.appendChild(dettagli);
+    const info = document.createElement('p');
+    info.classList.add('underline');
+    info.textContent = "Clicca per più informazioni";
+    info.addEventListener('click', apriInfo);
+    divcorso.appendChild(info);
+}
+
+function keyUp() {
+    for (var i = 0; i < courses.length; i++) {
+        const s = courses[i].querySelector('span h3').textContent;
+        if (s.toLowerCase().indexOf(input.value.toLowerCase()) !== -1) {
+            courses[i].classList.remove('hidden');
+        }
+        else {
+            courses[i].classList.add('hidden');
+        }
+    }
+}
+
+const input = document.querySelector('input');
+input.addEventListener('keyup', keyUp);
+const courses = document.querySelectorAll('.corsi div');
+
+
+function apriInfo(event) {
+    const informazioni = event.currentTarget;
+    informazioni.removeEventListener('click', apriInfo);
+
+    const dettagli = informazioni.parentNode.querySelector('.hidden');
+    dettagli.classList.remove('hidden');
+
+    informazioni.textContent = "Mostra meno dettagli";
+    informazioni.addEventListener('click', chiudiInfo);
+}
+
+function chiudiInfo(event) {
+    const informazioni = event.currentTarget;
+    informazioni.removeEventListener('click', chiudiInfo);
+
+    const dettagli = informazioni.parentNode.querySelector('.inf');
+    dettagli.classList.add('hidden');
+
+    informazioni.textContent = "Clicca per più informazioni";
+    informazioni.addEventListener('click', apriInfo);
+}
+
+const section_preferiti = document.querySelector('.all_preferiti');
+const h1 = document.createElement('h1');
+h1.textContent = "Preferiti:";
+section_preferiti.appendChild(h1);
+
+const divcorsi_preferiti = document.createElement('div');
+divcorsi_preferiti.classList.add('corsi_preferiti');
+section_preferiti.appendChild(divcorsi_preferiti);
+
+function onFavorites(event) {
+
+    const button = event.currentTarget;
+    button.textContent = "Rimuovi dai preferiti";
+    button.removeEventListener('click', onFavorites);
+    button.addEventListener('click', deleteFavorites);
+
+
+    const divcorso_preferiti = document.createElement('div');
+    divcorso_preferiti.classList.add("items");
+    divcorso_preferiti.id = button.parentNode.parentNode.id;
+    if (divcorsi_preferiti.childNodes.length === 0) {
+        section_preferiti.classList.remove('hidden');
+    }
+    divcorsi_preferiti.appendChild(divcorso_preferiti);
+
+
+    const span_preferiti = document.createElement('span');
+    span_preferiti.classList.add('row_preferiti');
+    divcorso_preferiti.appendChild(span_preferiti);
+    const titolo_preferiti = document.createElement('h3');
+    titolo_preferiti.textContent = button.parentNode.querySelector('h3').textContent;
+    span_preferiti.appendChild(titolo_preferiti);
+    const button_preferiti = document.createElement('span');
+    button_preferiti.classList.add('button');
+    button_preferiti.textContent = "Rimuovi dai preferiti";
+    button_preferiti.addEventListener('click', removeToFavorites);
+    span_preferiti.appendChild(button_preferiti);
+    const img_preferiti = document.createElement('img');
+    img_preferiti.src = button.parentNode.parentNode.querySelector('img').src;
+    divcorso_preferiti.appendChild(span_preferiti);
+    divcorso_preferiti.appendChild(img_preferiti);
+}
+
+function removeToFavorites(event) {
+    const new_button = event.currentTarget;
+
+    const preferiti = new_button.parentNode.parentNode;
+    preferiti.remove();
+    const divcorsi_preferiti = document.querySelector('.corsi_preferiti');
+    if (divcorsi_preferiti.childNodes.length === 0) {
+        section_preferiti.classList.add('hidden');
+    }
+
+    const items = document.querySelectorAll(".corsi div");
+    for (const item of items) {
+        if (preferiti.id === item.id) {
+            const button = item.querySelector('span span');
+            button.textContent = "Aggiungi ai preferiti";
+            button.addEventListener('click', onFavorites);
+            button.removeEventListener('click', deleteFavorites);
+        }
+
+    }
+
+}
+
+function deleteFavorites(event) {
+    const button = event.currentTarget;
+    const preferiti = button.parentNode.parentNode;
+
+    const items = document.querySelectorAll(".all_preferiti div");
+    for (const item of items) {
+        if (preferiti.id === item.id) {
+            item.remove();
+            button.textContent = "Aggiungi ai preferiti";
+            button.addEventListener('click', onFavorites);
+            button.removeEventListener('click', deleteFavorites);
+        }
+    }
+
+    const divcorsi_preferiti = document.querySelector('.corsi_preferiti');
+    if (divcorsi_preferiti.childNodes.length === 0) {
+        section_preferiti.classList.add('hidden');
+    }
+
+}
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function onJson (json) {
+
+    const risultati = document.querySelector('#risultati1');
+    risultati.innerHTML= ''; //se ci sono risultati della vecchia query li cancello
+
+    const doc = json;
+    console.log(doc);
+    const description = doc.player[0].strDescriptionIT;
+
+    //Creo la descrizione
+    const descrizione = document.createElement('p');
+    descrizione.textContent = description;
+
+    //Aggiugno la descrizione alla sezione risultati
+    risultati.appendChild(descrizione);
+}
+
+
+function onResponse (response) {
+    return response.json();
+}
+
+function search (event) {
+    event.preventDefault(); //per impedire il comportamento del form attraverso il bottone
+    
+    //Prendo il valore del campo di testo
+    const player_input = document.querySelector('#player'); 
+    const player_value = encodeURIComponent(player_input.value);
+
+    //Mi preparo la richiesta
+    rest_url = 'https://www.thesportsdb.com/api/v1/json/1/searchplayers.php?p=' + player_value;
+
+    //Effetuo la richiesta
+    fetch(rest_url).then(onResponse).then(onJson);
+}
+
+//Aggiungo l'EventListener per il form (api_1)
+const form = document.querySelector('#api_1 form');
+form.addEventListener('submit', search); 
+
+
+//---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+function onJsonTracks(json) {
+    const risultati = document.querySelector('#risultati2');
+    risultati.innerHTML= ''; //se ci sono risultati della vecchia query li cancello
+    for(track of json.albums[0].tracks.items) {
+    const p = document.createElement('p');
+    p.textContent = track.name;
+    risultati.appendChild(p);
+    }
+}
+
+
+function onJsonAlbums (json) {
+    console.log(json);
+    fetch('https://api.spotify.com/v1/albums?ids=' + json.albums.items[0].id, {
+        headers: 
+        {
+            'Authorization': 'Bearer ' + token
+        }
+    }).then(onResponse).then(onJsonTracks);
+}
+
+function search_2 (event){
+    event.preventDefault();
+
+    const album_input = document.querySelector('#album');
+    const album_value = encodeURIComponent(album_input.value);
+
+    //Eseguo richiesta asincrona alle REST API di Spotify
+    fetch("https://api.spotify.com/v1/search?type=album&q=" + album_value, 
+    {
+        headers:
+        {
+            'Authorization' : 'Bearer ' + token
+        }
+
+    }).then(onResponse).then(onJsonAlbums);
+}
+
+
+function onTokenJson(json) {
+    token = json.access_token;
+}
+
+function onTokenResponse(response) {
+    return response.json();
+}
+
+//Richiesta per ottenere il token (mi servono le credenziali che ottengo in fase di registrazione )
+const client_id = 'e4f0bbe64bf246be98c4533a6c9b0094';
+const client_secret = 'b3803c2eae9b4bc392c56034bbbf9242';
+
+//Dichiaro la variabile dove mettere il token
+let token;
+
+
+//Fetch al server di autenticazione di Spotify
+fetch("https://accounts.spotify.com/api/token",
+{
+    //specifico il metodo
+    method: "post",
+    body: 'grant_type=client_credentials',
+    headers: 
+    {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'Authorization': 'Basic ' + btoa(client_id + ':' + client_secret)
+    }
+}).then(onTokenResponse).then(onTokenJson);
+
+//Aggiungo l'EventListener al form (api_2)
+const form2 = document.querySelector('#api_2 form');
+form2.addEventListener('submit', search_2); 
